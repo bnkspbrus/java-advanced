5a09e0fe4ed5e2c8001b3ae682ea731b77d022cb
bnkspbrus <bnkspbrus@gmail.com>
2022-03-10 12:12:45 +0300

    fix StudentDB.java

--- java-solutions/info/kgeorgiy/ja/barsukov/student/StudentDB.java
+++ java-solutions/info/kgeorgiy/ja/barsukov/student/StudentDB.java
@@ -13,9 +13,9 @@ import java.util.stream.Collectors;
 public class StudentDB implements StudentQuery {
     private static final Comparator<Student> SORT_BY_NAME =
             // :NOTE: Упростить
-            Comparator.comparing(Student::getLastName, Comparator.reverseOrder())
-                    .thenComparing(Student::getFirstName, Comparator.reverseOrder())
-                    .thenComparingInt(Student::getId);
+            Comparator.comparing(Student::getLastName).thenComparing(Student::getFirstName).reversed().thenComparingInt(Student::getId);
+
+    private static final Comparator<Student> COMPARE_ID = Comparator.comparingInt(Student::getId);
 
     @Override
     public List<String> getFirstNames(final List<Student> students) {
@@ -49,12 +49,12 @@ public class StudentDB implements StudentQuery {
     @Override
     public String getMaxStudentFirstName(final List<Student> students) {
         // :NOTE: Дубли
-        return students.stream().max(Comparator.comparingInt(Student::getId)).map(Student::getFirstName).orElse("");
+        return students.stream().max(COMPARE_ID).map(Student::getFirstName).orElse("");
     }
 
     @Override
     public List<Student> sortStudentsById(final Collection<Student> students) {
-        return sortCollectionByComparator(students, Comparator.comparingInt(Student::getId));
+        return sortCollectionByComparator(students, COMPARE_ID);
     }
 
     @Override
@@ -62,37 +62,39 @@ public class StudentDB implements StudentQuery {
         return sortCollectionByComparator(students, SORT_BY_NAME);
     }
 
-    private List<Student> sortCollectionByComparator(final Collection<Student> students, final Comparator<Student> comparator) {
+    private List<Student> sortCollectionByComparator(final Collection<Student> students,
+            final Comparator<Student> comparator) {
         return students.stream().sorted(comparator).collect(Collectors.toList());
     }
 
-    private List<Student> findStudentsByPredicate(final Collection<Student> students, final Predicate<Student> predicate) {
+    private List<Student> findStudentsByPredicate(final Collection<Student> students,
+            final Predicate<Student> predicate) {
         return students.stream().filter(predicate).sorted(SORT_BY_NAME).collect(Collectors.toList());
+    }
+
+    private <T> Predicate<Student> propertyPredicate(final Function<Student, T> actual, final T expected) {
+        return student -> actual.apply(student).equals(expected);
     }
 
     // :NOTE: Дубли
     @Override
     public List<Student> findStudentsByFirstName(final Collection<Student> students, final String name) {
-        return findStudentsByPredicate(students, student -> student.getFirstName().equals(name));
+        return findStudentsByPredicate(students, propertyPredicate(Student::getFirstName, name));
     }
 
     @Override
     public List<Student> findStudentsByLastName(final Collection<Student> students, final String name) {
-        return findStudentsByPredicate(students, student -> student.getLastName().equals(name));
+        return findStudentsByPredicate(students, propertyPredicate(Student::getLastName, name));
     }
 
     @Override
     public List<Student> findStudentsByGroup(final Collection<Student> students, final GroupName group) {
-        return findStudentsByPredicate(students, groupPredicate(group));
-    }
-
-    private Predicate<Student> groupPredicate(final GroupName group) {
-        return student -> student.getGroup() == group;
+        return findStudentsByPredicate(students, propertyPredicate(Student::getGroup, group));
     }
 
     @Override
     public Map<String, String> findStudentNamesByGroup(final Collection<Student> students, final GroupName group) {
-        return students.stream().filter(groupPredicate(group)).collect(
+        return students.stream().filter(propertyPredicate(Student::getGroup, group)).collect(
                 Collectors.toMap(Student::getLastName, Student::getFirstName, BinaryOperator.minBy(String::compareTo)));
     }
 }
