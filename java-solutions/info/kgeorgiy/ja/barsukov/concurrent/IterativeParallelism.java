ca5df46171c1399cc64f2d87e47d73311c44166d
bnkspbrus <bnkspbrus@gmail.com>
2022-03-31 14:01:59 +0300

    adding IterativeParrallelism.java

---
+++ java-solutions/info/kgeorgiy/ja/barsukov/concurrent/IterativeParallelism.java
@@ -0,0 +1,93 @@
+package info.kgeorgiy.ja.barsukov.concurrent;
+
+import info.kgeorgiy.java.advanced.concurrent.ScalarIP;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+/**
+ *
+ */
+public class IterativeParallelism implements ScalarIP {
+    private static <T> List<Stream<? extends T>> split(final int threads, final List<? extends T> values) {
+        final int partsSize = values.size() / threads;
+        int remainder = values.size() % threads;
+        final List<Stream<? extends T>> parts = new ArrayList<>(threads);
+        int position = 0;
+        while (position < values.size()) {
+            final int partSize = partsSize + (remainder > 0 ? 1 : 0);
+            parts.add(values.subList(position, position + partSize).stream());
+            position += partSize;
+            if (remainder > 0) {
+                remainder--;
+            }
+        }
+        return parts;
+    }
+
+    private static <T, R> R execute(final List<Stream<? extends T>> parts, final Function<Stream<? extends T>, R> process,
+            final Function<Stream<? extends R>, R> reduce) throws InterruptedException {
+        final List<R> results = new ArrayList<>(Collections.nCopies(parts.size(), null));
+        final List<Thread> threads = createThreads(parts, process, results);
+        joinAll(threads);
+        return reduce.apply(results.stream());
+    }
+
+    private static <T, M> List<Thread> createThreads(final List<Stream<? extends T>> parts,
+            final Function<Stream<? extends T>, M> process, final List<M> results) {
+        return IntStream.range(0, parts.size()).mapToObj(i -> {
+            final Thread thread = new Thread(() -> results.set(i, process.apply(parts.get(i))));
+            thread.start();
+            return thread;
+        }).toList();
+    }
+
+    private static void joinAll(final List<Thread> threads) throws InterruptedException {
+        for (final Thread thread : threads) {
+            thread.join();
+        }
+    }
+
+    private static  <T, R> R execute(final int threads, final List<? extends T> values,
+            final Function<Stream<? extends T>, R> process,
+            final Function<Stream<? extends R>, R> reduce) throws InterruptedException {
+        return execute(split(threads, values), process, reduce);
+    }
+
+    private <T> T execute(final int threads, final List<? extends T> values,
+            final Function<Stream<? extends T>, T> process) throws InterruptedException {
+        return execute(split(threads, values), process, process);
+    }
+
+    @Override
+    public <T> T maximum(final int threads, final List<? extends T> values,
+            final Comparator<? super T> comparator) throws InterruptedException {
+        return execute(threads, values, stream -> stream.max(comparator).orElse(null));
+    }
+
+    @Override
+    public <T> T minimum(final int threads, final List<? extends T> values,
+            final Comparator<? super T> comparator) throws InterruptedException {
+        return execute(threads, values, stream -> stream.min(comparator).orElse(null));
+    }
+
+    @Override
+    public <T> boolean all(final int threads, final List<? extends T> values,
+            final Predicate<? super T> predicate) throws InterruptedException {
+        return execute(threads, values, stream -> stream.allMatch(predicate),
+                stream -> stream.allMatch(Boolean::booleanValue));
+    }
+
+    @Override
+    public <T> boolean any(final int threads, final List<? extends T> values,
+            final Predicate<? super T> predicate) throws InterruptedException {
+        return execute(threads, values, stream -> stream.anyMatch(predicate),
+                stream -> stream.anyMatch(Boolean::booleanValue));
+    }
+}
